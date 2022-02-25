e1e0039ebbca37e5d866cc24a4daaeb387e9db2a
Sam Firminger <samfirminger@Sams-MacBook-Pro.local>
2022-02-25 15:55:12 +0000

    adding files

---
+++ src/allergies.ts
@@ -0,0 +1,13 @@
+export class Allergies {
+  constructor() {
+    throw new Error('Implement this method!');
+  }
+
+  public list(): string[] {
+    throw new Error('Implement this method!');
+  }
+
+  public allergicTo() {
+    throw new Error('Implement this method!');
+  }
+}
