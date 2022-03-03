7f4d2e93cf4ec17fe3049771ec952dea29bec0d3
mjgreen145 <mjgreen145@gmail.com>
2022-03-03 15:00:25 +0000

    Implementation by Matt, Phil & Tijani

--- src/allergies.ts
+++ src/allergies.ts
@@ -1,13 +1,49 @@
+type Allergy = { allergen: string; score: number };
+
+const allergyScores: Array<Allergy> = [
+  { allergen: 'eggs', score: 1 },
+  { allergen: 'peanuts', score: 2 },
+  { allergen: 'shellfish', score: 4 },
+  { allergen: 'strawberries', score: 8 },
+  { allergen: 'tomatoes', score: 16 },
+  { allergen: 'chocolate', score: 32 },
+  { allergen: 'pollen', score: 64 },
+  { allergen: 'cats', score: 128 },
+];
+
 export class Allergies {
-  constructor() {
-    throw new Error('Implement this method!');
+  allergies: string[];
+
+  constructor(score: number) {
+    this.allergies = Allergies.getAllergies(score);
+  }
+
+  private static getAllergies(score: number): string[] {
+    const nextPowerOf2 = 2 * Math.max(...allergyScores.map(({ score }) => score));
+
+    return allergyScores
+      .sort((a, b) => b.score - a.score)
+      .reduce(
+        (acc, allergy) => {
+          const isAllergic = allergy.score <= acc.remainingScore;
+
+          if (isAllergic) {
+            return {
+              allergies: [allergy.allergen, ...acc.allergies],
+              remainingScore: acc.remainingScore - allergy.score,
+            };
+          }
+          return acc;
+        },
+        { allergies: [], remainingScore: score % nextPowerOf2 }
+      ).allergies;
   }
 
   public list(): string[] {
-    throw new Error('Implement this method!');
+    return this.allergies;
   }
 
-  public allergicTo() {
-    throw new Error('Implement this method!');
+  public allergicTo(allergen: string): boolean {
+    return this.allergies.includes(allergen);
   }
 }
